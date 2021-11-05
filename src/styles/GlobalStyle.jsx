02e2270f6060be30bd861fa46cfd14ef31837228
Karoline Barreto <karolinedbarreto@gmail.com>
2021-11-05 00:23:35 -0300

    Fix font-family

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,12 +1,12 @@
+import React from "react";
 import { createGlobalStyle } from "styled-components";
+import { Helmet } from "react-helmet";
 
 import { BreakpointSize, breakAt } from "./Breakpoints";
 
 const colorYellow = "#ffc107";
 
 const GlobalStyle = createGlobalStyle`
-    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap");
-
     html{
       font-family: "Poppins", sans-serif;
       font-weight: 300;
@@ -78,5 +78,17 @@ const GlobalStyle = createGlobalStyle`
       }
     }
 `;
+
+const GlobalStyleComposed = () => (
+  <>
+    <GlobalStyle />
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+  </>
+);
 
 export default GlobalStyle;
